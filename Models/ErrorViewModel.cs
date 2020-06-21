8cd05130d55e8364a29d026c4c225a757d395c82
ROYALSYS <royalsysvcapy@gmail.com>
2020-06-21 08:51:42 -0400

    create a mvc proyect in vscode

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pysanga.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
